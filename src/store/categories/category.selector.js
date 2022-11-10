0e48df68bd20d67b80a9c580145587b0c13a08e2
Marija <marija@dailyart.eu>
2022-11-10 16:54:57 +0200

    redux-thunk

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -16,3 +16,9 @@ export const selectCategoriesMap = createSelector(
     }, {})
 );
 
+export const selectCategoriesIsLoading  = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.isLoading
+
+)
+
